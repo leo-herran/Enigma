11b8a5ddd32ce530ec57172ae0c5a9cb65988f9a
Leo Baran <leobaran@cs.washington.edu>
2015-06-17 12:36:18 -0700

    character validator.

---
+++ pythonVersion/charChecker.py
@@ -0,0 +1,11 @@
+# !, ', (, ), (comma), :, ;, ?	
+okayCharacters = [33, 39, 40, 41, 44, 46, 58, 59, 63];
+
+	
+def checkCharAcceptable(c):
+	cOrd = ord(c);
+	return cOrd in okayCharacters;
+
+def checkIsLetter(c):
+	cOrd = ord(c);
+	return cOrd in range(97, 123);
