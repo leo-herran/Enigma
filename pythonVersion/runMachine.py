baaa6efdd2c691ed811a55eb76ec44addea81409
Leo Baran <leobaran@cs.washington.edu>
2015-07-09 11:44:49 -0700

    tab weirdness.

--- pythonVersion/runMachine.py
+++ pythonVersion/runMachine.py
@@ -1,76 +1,82 @@
 from machine import Machine
 
 def encodeOrDecode():
-	print("encode or decode? (e/d)");
-	encode = input()[0];
-	if(encode != 'e' and encode != 'd'): 
-		print("Sorry?");
-		return encodeOrDecode();
-	else:
-		if encode == 'e':
-			return True;
-		else:
-			return False;
+    print("encode or decode? (e/d)");
+    encode = input()[0];
+    if(encode != 'e' and encode != 'd'): 
+        print("Sorry?");
+        return encodeOrDecode();
+    else:
+        if encode == 'e':
+            return True;
+        else:
+            return False;
 
 #change numbers to #
 def censorNumbers(word):
-	for i in range(0, len(word)):
-		c = ord(word[i]);
-		if c >= 48 and c <= 57:
-			newWord = word[0:i] + "#" + word[(i+1):];
-			word = newWord;
-					
-	return word;
+    for i in range(0, len(word)):
+        c = ord(word[i]);
+        if c >= 48 and c <= 57:
+            newWord = word[0:i] + "*" + word[(i+1):];
+            word = newWord;
+                            
+    return (word, numbers);
 
 def transformMessage(machine, message, encode):
-	for i in range(0, len(message)):
-		word = message[i];
-		transformedWord = machine.getTransformedString(word, encode);
-		message[i] = transformedWord;
+    for i in range(0, len(message)):
+        word = message[i];
+        transformedWord = machine.getTransformedString(word, encode);
+        message[i] = transformedWord;
 
-	return message;			
+    return message;			
 	
 def printMessage(message):
-	for word in message:
-		print(word + " ", end="");
+    for word in message:
+        print(word + " ", end="");
 
-	print("");
+    print("");
 
 def runInputAndOutput(machine):
-	encode = encodeOrDecode();
-	print("Enter your message.");
-	message = input().split();
+    encode = encodeOrDecode();
+    print("Enter your message.");
+    message = input().split();
 
-	#convert to lower case. 
-	for word in message:
-		message[message.index(word)] = word.lower();
-	
-	#for now we can just censor numbers. 
-	for i in range(1, len(message)):
-		word = message[i];
-		if not word.isalpha():
-			newWord = censorNumbers(word);
-			message[i] = newWord; 
+    #convert to lower case. 
+    for word in message:
+        message[message.index(word)] = word.lower();
+ 
+    #for now we can just censor numbers. 
+    #change numbers to special character (*) and encrypt them. 
+    
+    for i in range(1, len(message)):
+        word = message[i];
+        if not word.isalpha():
+            wordData = censorNumbers(word); 
+            newWord = wordData[0];
+            message[i] = newWord; 
+            messageNumbers.append(wordData[1]);
 
-	message.append("<>"); #end of message signal.	
-	message = transformMessage(machine, message, encode);
-	message.remove("<>");
-	printMessage(message);
+
+    message.append("<>"); #end of message signal.	
+    message = transformMessage(machine, message, encode);
+    message.remove("<>");
+    printMessage(message);
+    #print(messageNumbers);
 
 
 def main():
-	m = Machine(False);
-	answer = "y";
-	while(answer == "y" or answer == "secret"): 
-		if(answer == "secret"):
-			m = Machine(True);
-		
-		runInputAndOutput(m);
-		print("Use the enigma machine again? (y/n)");
-		answer = input();
-	
-	print("later.");
+    m = Machine(False);
+    answer = "y";
+    while(answer == "y" or answer == "secret"): 
+        if(answer == "secret"):
+            m = Machine(True);
+        
+        runInputAndOutput(m);
+        print("Use the enigma machine again? (y/n)");
+        answer = input();
+
+    print("later.");
 	
 
 if __name__ == "__main__":
-	main();
+    main();
